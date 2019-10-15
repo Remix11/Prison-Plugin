ab739c169feda53c1d11e385ff939d8e7c1e7a3b
Remix11 <56566851+Remix11@users.noreply.github.com>
2019-10-14 20:02:51 -0400

    Add files via upload

---
+++ Plugin Template/src/me/divinius/MainClass.java
@@ -0,0 +1,24 @@
+package me.divinius;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R3.Overridden;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainClass extends JavaPlugin {
+
+	@Overridden
+	public void onEnable() {
+		
+	}
+	
+	@Overridden
+	public void onDisable() {
+		
+	}
+	
+		
+		
+}
+	

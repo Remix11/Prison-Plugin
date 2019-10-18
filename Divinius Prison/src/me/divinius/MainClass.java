ed0da6acf378c19530ca7eff142484efccd17e69
Remix11 <56566851+Remix11@users.noreply.github.com>
2019-10-18 11:09:45 -0400

    Update MainClass.java

--- Divinius Prison/src/me/divinius/MainClass.java
+++ Divinius Prison/src/me/divinius/MainClass.java
@@ -3,6 +3,9 @@
  */
 package me.divinius;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
@@ -12,6 +15,8 @@ import java.util.Random;
  *
  */
 public class MainClass extends JavaPlugin {
+	
+	int numberofcoins = 0;
 	
 	
 	
@@ -28,22 +33,26 @@ public class MainClass extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		
 		
-		if (cmd.getName().equalsIgnoreCase("cf") && sender instanceof Player)
+		if (cmd.getName().equalsIgnoreCase("cl") && sender instanceof Player) {
 			
 			Player player = (Player) sender;
 		
 			Random rand = new Random();
 	
-			int coins = rand.nextInt(-50,50);
+			int coins = rand.nextInt(50);
 			
-			player.sendMessage(rand.nextInt() + "coins")
+			player.sendMessage(rand.nextInt() + "coins");
+				
+			numberofcoins = numberofcoins + coins;
 			
 		
-		return true
+		return true;
 	
 	
 	}
 	
-	return false
+	return false;
+
+ }
 
 }
